4ca08be1c4af91b4aa9e46c88cb045bb805d7668
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-09-15 22:20:12 +0000

    4_queries directory added

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, avg(completed_at - started_at) AS  average_assistance_time
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN assistance_requests ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
